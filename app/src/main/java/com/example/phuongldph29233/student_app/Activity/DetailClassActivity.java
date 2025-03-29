530f5db75ef084fb5a809e884c80ad55d7b8cc2b
ndd2509 <dinhdung250998@gmail.com>
2025-03-30 01:10:12 +0700

    class

--- app/src/main/java/com/example/phuongldph29233/student_app/Activity/DetailClassActivity.java
+++ app/src/main/java/com/example/phuongldph29233/student_app/Activity/DetailClassActivity.java
@@ -1,7 +1,18 @@
 package com.example.phuongldph29233.student_app.Activity;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DetailClassActivity extends AppCompatActivity {
+import com.example.phuongldph29233.student_app.Domain.Branch;
+import com.example.phuongldph29233.student_app.Domain.Class;
+import com.example.phuongldph29233.student_app.Helper.DatabaseHelper;
 
+public class DetailClassActivity extends AppCompatActivity {
+    private TextView txtMaLop, txtTenLop, txtKhoa, txtGiangVien, txtNamHoc;
+    private Button btnEdit, btnDelete, btnBack;
+    private String maLop,tenLop,khoa,giangVien,namHoc;
+    private DatabaseHelper<Class> classDatabaseHelper;
+    private DatabaseHelper<Branch> branchDatabaseHelper;
 }
